c7c2997de2cbad730dbc1bb67d8cb28869a7909c
Vitalii Balabanov <tertiumnon@gmail.com>
2025-02-26 18:50:12 +0100

    add themes

---
+++ src/app/components/theme/theme.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, signal } from "@angular/core";
+import { Theme } from "./theme";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ThemeService {
+  themes = Object.values(Theme);
+  theme = signal<string>("");
+
+  constructor() {
+    this.theme.set(localStorage.getItem("theme") || Theme.Coder);
+  }
+
+  setTheme(theme: Theme) {
+    this.theme.set(theme);
+    localStorage.setItem("theme", theme);
+  }
+}
