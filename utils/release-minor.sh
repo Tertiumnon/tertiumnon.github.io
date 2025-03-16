b9c7ad6f115be7603b780a483ac8388b87c8fd7f
Vitalii Balabanov <tertiumnon@gmail.com>
2025-03-16 14:41:29 +0100

    fix chlpr

--- utils/release-minor.sh
+++ utils/release-minor.sh
@@ -1,10 +1,11 @@
-npm version minor
-npm run build:gh
-git add --all
-git commit --amend -C HEAD
-git push
-git checkout master
-git merge develop
-git push
-git push --tags
-git checkout develop
+npm version minor &&
+npm run build:gh &&
+git add --all &&
+git commit --amend -C HEAD &&
+git push &&
+git checkout master &&
+git pull &&
+git merge develop &&
+git push &&
+git push --tags &&
+git checkout develop &&
