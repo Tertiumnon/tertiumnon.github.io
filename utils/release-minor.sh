788f1f5f9f3cb19c19e924aca1ee2a3143274e93
Vitalii Balabanov <tertiumnon@gmail.com>
2025-03-16 14:49:07 +0100

    minor fix

--- utils/release-minor.sh
+++ utils/release-minor.sh
@@ -1,11 +1,11 @@
-npm version minor &&
-npm run build:gh &&
-git add --all &&
-git commit --amend --no-edit &&
-git push -f &&
-git checkout master &&
-git pull &&
-git merge develop &&
-git push &&
-git push --tags &&
+npm version minor
+npm run build:gh
+git add --all
+git commit --amend --no-edit
+git push -f
+git checkout master
+git pull
+git merge develop
+git push
+git push --tags
 git checkout develop
