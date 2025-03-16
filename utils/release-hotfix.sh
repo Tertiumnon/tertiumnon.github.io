c594ce30abdc58e2030b06c4e1aaf90072805f77
Vitalii Balabanov <tertiumnon@gmail.com>
2025-03-16 14:42:35 +0100

    1.26.3

--- utils/release-hotfix.sh
+++ utils/release-hotfix.sh
@@ -1,10 +1,10 @@
-npm version patch
-npm run build:gh
-git add --all
-git commit --amend --no-edit
-git push
-git push --tags
-git checkout develop
-git rebase origin/master
-git push
+npm version patch &&
+npm run build:gh &&
+git add --all &&
+git commit --amend --no-edit &&
+git push -f &&
+git push --tags &&
+git checkout develop &&
+git rebase origin/master &&
+git push &&
 git checkout master
