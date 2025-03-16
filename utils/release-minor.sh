9bdc3dcc5c1e69986d6c772957f1434c918a8027
Vitalii Balabanov <tertiumnon@gmail.com>
2025-03-16 14:55:45 +0100

    1.29.0

--- utils/release-minor.sh
+++ utils/release-minor.sh
@@ -1,12 +1,2 @@
 npm version minor
 git push
-# npm run build:gh
-# git add --all
-# git commit --amend --no-edit
-# git push -f
-# git checkout master
-# git pull
-# git merge develop
-# git push
-# git push --tags
-# git checkout develop
