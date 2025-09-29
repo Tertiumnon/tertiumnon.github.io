590ae70b1eef50be062575acf3d7e763db4897c3
Vitalii Balabanov <tertiumnon@gmail.com>
2025-09-29 15:17:17 +0200

    fix(component): remove unused RouterLink import

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -5,8 +5,7 @@ import { IProject } from '../../entities/project/project.interface';
 @Component({
     selector: 'app-project-card',
     templateUrl: './project-card.component.html',
-    styleUrls: ['./project-card.component.less'],
-    imports: [RouterLink]
+  styleUrls: ['./project-card.component.less']
 })
 export class ProjectCardComponent {
   @Input() project?: IProject;
